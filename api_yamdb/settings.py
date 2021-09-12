19bad01a394e47734cf4eec3e71c6644a8a06026
Vadim <kv1o@yandex.ru>
2021-09-12 12:04:56 +0300

    add temporary vars for tests

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -13,12 +13,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET', 'test')
+SECRET_KEY = os.environ.get('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ.get('SERVER_HOST', 'test'), 'localhost']
+ALLOWED_HOSTS = [os.environ.get('SERVER_HOST'), 'localhost']
 
 # Application definition
 
